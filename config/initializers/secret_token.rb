bbd48c1c698bd62fd5e4900a5e1591e51058ee23
Adam Krebs <amk528@cs.nyu.edu>
2012-08-18 02:00:21 -0400

    initial rails-api gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FTApi::Application.config.secret_token = 'c91722562d90e014a5c027960dbba852c37485bab2280576499667987f65c09ee07e3b547c1aba9345ccc4c51c9bd1ca3648782ec3d643fe44bfab56500f3532'
