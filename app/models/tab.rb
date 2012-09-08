4e7f0c34770efa37ffc52de18965c468d72665a2
Adam Krebs <amk528@cs.nyu.edu>
2012-09-08 04:53:20 -0400

    clean out transaction model

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,10 +1,11 @@
 class Tab < ActiveRecord::Base
   belongs_to :customer
   attr_protected :balance
+  validates_numericality_of :balance
 
   before_save :insert_transaction_record
 
   def insert_transaction_record
-    #Transaction.new(message: serialze :balance.changes)
+    Transaction.create(message: "new balance is #{:balance}, changes are #{:balance.changes}")
   end
 end
