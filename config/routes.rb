8eab108bfdc1b984aa53a50ec3a647f633f619f1
Adam Krebs <amk528@cs.nyu.edu>
2012-10-22 01:52:06 -0400

    exclude update from transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 FTApi::Application.routes.draw do
-  resources :transactions, except: [:new, :edit]
+  resources :transactions, except: [:new, :edit, :update]
 
   resources :farms, except: [:show, :edit, :delete]
 
   resources :customers, except: :edit do
-    resources :transactions, except: [:edit, :delete]
+    resources :transactions, except: [:edit, :update, :delete]
   end
 
   # The priority is based upon order of creation:
