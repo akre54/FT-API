4a65410b4050f4ae9929d783cd18ec792140db29
Adam Krebs <amk528@cs.nyu.edu>
2012-10-21 23:53:11 -0400

    farm has_many transactions

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -4,5 +4,6 @@ class Farm < ActiveRecord::Base
   validates_presence_of :email, :farm_name, :pass, :salt
   
   has_and_belongs_to_many :customers
+  has_many :transactions, through: :customers
 
 end
