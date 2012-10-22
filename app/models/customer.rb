134eaa133d91c4733c3a48cf7523ac0da46e6cf2
Adam Krebs <amk528@cs.nyu.edu>
2012-10-21 23:30:29 -0400

    remove set balance on customer model. should only be done through a transaction

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,11 +17,4 @@ class Customer < ActiveRecord::Base
     current_tab.balance
   end
 
-  # fix this
-  def balance=(newAmt)
-    tab = self.tabs.find_or_create_by_farm_id 2
-    tab.balance = newAmt
-    tab.save!
-  end
-
 end
