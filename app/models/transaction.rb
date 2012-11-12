216f10f40acb9bf51b938a1e34aba6294a103e5e
Adam Krebs <amk528@cs.nyu.edu>
2012-11-11 20:56:20 -0500

    add greater-than-zero constraint on transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,7 +6,7 @@ class Transaction < ActiveRecord::Base
     belongs_to :venue
 
     validates_presence_of :amount, :transaction_type, :farm_id, :customer_id
-    validates_numericality_of :amount
+    validates_numericality_of :amount, greater_than_or_equal_to: 0
     validates_inclusion_of :transaction_type, in: %w(deposit withdrawal)
 
     after_create :update_balance
