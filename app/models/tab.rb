0d9968e41af39b25893390a80a7f8d4f4c7ab2b0
Adam Krebs <amk528@cs.nyu.edu>
2012-11-12 00:35:48 -0500

    validates_uniqueness_of constraint for tab

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -2,5 +2,7 @@ class Tab < ActiveRecord::Base
   belongs_to :customer
   belongs_to :farm
   attr_protected :balance
+
   validates_numericality_of :balance, greater_than_or_equal_to: 0
+  validates_uniqueness_of :customer_id, scope: :farm_id
 end
