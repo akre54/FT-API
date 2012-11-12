15c468a43f28248e0c78459f8544ae5856ba2eb9
Adam Krebs <amk528@cs.nyu.edu>
2012-11-11 20:55:33 -0500

    modify migration to use decimal type instead of float. duh

--- db/migrate/20121022024623_change_transaction_amount_to_float.rb
+++ db/migrate/20121022024623_change_transaction_amount_to_float.rb
@@ -1,9 +1,9 @@
 class ChangeTransactionAmountToFloat < ActiveRecord::Migration
   def up
-    change_column :transactions, :amount, :float, :null => false
+    change_column :transactions, :amount, :decimal, precision: 10, scale: 2, null: false
   end
 
   def down
-    change_column :transactions, :amount, :string, :null => false
+    change_column :transactions, :amount, :string, null: false
   end
 end
