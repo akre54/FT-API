231e582ff0488111bd38b35dab6795663a767716
Adam Krebs <amk528@cs.nyu.edu>
2012-11-12 01:14:00 -0500

    add a negativity validator to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,6 +8,7 @@ class Transaction < ActiveRecord::Base
     validates_presence_of :amount, :transaction_type, :farm_id, :customer_id
     validates_numericality_of :amount, greater_than_or_equal_to: 0
     validates_inclusion_of :transaction_type, in: %w(deposit withdrawal)
+    validate :negative_validator
 
     after_create :update_balance
 
@@ -16,6 +17,12 @@ class Transaction < ActiveRecord::Base
             self.customer.balance += self.amount
         else
             self.customer.balance -= self.amount
+        end
+    end
+
+    def negative_validator
+        if transaction_type == 'withdrawal' and amount > self.customer.balance
+            errors.add(:amount, 'cannot be more than customer balance')
         end
     end
 end
