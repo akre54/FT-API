c21a2f62dbef966fda33e68ebc2119cbf302acef
Adam Krebs <amk528@cs.nyu.edu>
2012-11-11 21:53:46 -0500

    cleanup tab

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,11 +1,5 @@
 class Tab < ActiveRecord::Base
   belongs_to :customer
   attr_protected :balance
-  validates_numericality_of :balance
-
-  before_save :insert_transaction_record
-
-  def insert_transaction_record
-    #Transaction.create(message: "new balance is #{:balance}, changes are #{:balance.changes}")
-  end
+  validates_numericality_of :balance, greater_than_or_equal_to: 0
 end
