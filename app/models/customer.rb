0677800c76a2d7f4d7a3e68358f2a99f9114392d
Adam Krebs <amk528@cs.nyu.edu>
2012-11-16 16:11:20 -0500

    make sure to downcase email so it can be searchable across database adapters

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,8 @@ class Customer < ActiveRecord::Base
   validates_presence_of :name, :crypted_pin, :salt, :email
   validates_uniqueness_of :email
 
+  before_save { |customer| customer.email = email.downcase }
+
   has_many :tabs , dependent: :destroy
   has_many :farms, through: :tabs
   has_many :transactions, through: :tabs
--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,6 +2,8 @@ class Farm < ActiveRecord::Base
   attr_accessible :email, :farm_name
   attr_protected :pass, :salt
   validates_presence_of :email, :farm_name, :pass, :salt
+
+  before_save { |farm| farm.email = email.downcase }
   
   has_many :tabs
   has_many :customers, through: :tabs
