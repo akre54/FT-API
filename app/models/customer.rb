ce023c25b26dca2234aee2090e8c8fa6e2178bd6
Adam Krebs <amk528@cs.nyu.edu>
2012-11-16 16:42:26 -0500

    property cleanup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,15 +2,14 @@ class Customer < ActiveRecord::Base
   attr_accessible :name, :email
   attr_protected :crypted_pin
 
-  validates_presence_of :name, :crypted_pin, :salt, :email
-  validates_uniqueness_of :email
-
-  before_save { |customer| customer.email = email.downcase }
-
   has_many :tabs , dependent: :destroy
   has_many :farms, through: :tabs
   has_many :transactions, through: :tabs
 
+  validates_presence_of :name, :crypted_pin, :salt, :email
+  validates_uniqueness_of :email
+
+  before_save { |customer| customer.email = email.downcase }
 
 =begin
   def balance
