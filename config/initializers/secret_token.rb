8bab06147db278870a4963867273d3c51386316e
Adam Krebs <amk528@cs.nyu.edu>
2013-03-27 12:48:14 -0400

    more secure secret_token initializer: dont store in github repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,20 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FTApi::Application.config.secret_token = 'c91722562d90e014a5c027960dbba852c37485bab2280576499667987f65c09ee07e3b547c1aba9345ccc4c51c9bd1ca3648782ec3d643fe44bfab56500f3532'
+
+if ENV["SECRET_TOKEN"].blank?
+  if Rails.env.production?
+    raise "You must set ENV[\"SECRET_TOKEN\"] in your app's config vars"
+  elsif Rails.env.test?
+    # Generate the key and test away
+    ENV["SECRET_TOKEN"] = FTApi::Application.config.secret_token = SecureRandom.hex(30)
+  else
+    config_file = File.expand_path(File.join(Rails.root, '/config/config.yml'))
+    config = YAML.load_file(config_file)
+    # Generate the key, set it for the current environment, update the yaml file and move on
+    ENV["SECRET_TOKEN"] = config[Rails.env]['SECRET_TOKEN'] = SecureRandom.hex(30)
+    File.open(config_file, 'w') { |file| file.write(config.to_yaml) }
+  end
+end
+
+FTApi::Application.config.secret_token = ENV["SECRET_TOKEN"]
