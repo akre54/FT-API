7fc7a69978dd679bb4c4b102cc5c43f3cee5f6b2
Adam Krebs <amk528@cs.nyu.edu>
2013-11-20 15:30:32 -0500

    comment out secret token check

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-if ENV["SECRET_TOKEN"].blank? && Rails.env.production?
-  raise "You must set ENV[\"SECRET_TOKEN\"] in your app's config vars"
-end
+# if ENV["SECRET_TOKEN"].blank? && Rails.env.production?
+#   raise "You must set ENV[\"SECRET_TOKEN\"] in your app's config vars"
+# end
 
 FTApi::Application.config.secret_token = ENV["SECRET_TOKEN"] || '1b6fbfee5b9318badcbb43dd34d03127b27cee1fa98d285638f35fc75537'
